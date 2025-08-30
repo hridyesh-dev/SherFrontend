a029a8d6bb93a0496a1a7cf191d6b6f97dd1b95b
hridyesh-dev <hridyesh.dev@gmail.com>
2025-08-30 17:52:48 +0530

    datatypes , rendring the list items , useState

---
+++ two_way_binding/src/main.jsx
@@ -0,0 +1,14 @@
+
+import { createRoot } from 'react-dom/client'
+
+// import App from './App.jsx'
+//import Appp from './Appp.jsx'
+import Aap from './Aap.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    {/* <App/>
+    <Appp/> */}
+    <Aap/>
+  </>,
+)
